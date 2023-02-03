92170629d96d83ac2a0f98c71fa4aee42f19879d
aminmshahnani <am.shahnani@gmail.com>
2023-02-03 20:14:42 +0330

    downloadly scrappe project
    This is my project for quera project, I choosed downloadly.it website to scrape by BS4

---
+++ downloadly.ir/README.md
@@ -0,0 +1,7 @@
+About the Downloadly.ir site:
+    Downloadly site is one of the largest online download app and learning courses.
+Purpose:
+    Extract all of app and link
+
+Site: 
+    downloadly.ir
---
+++ downloadly.ir/app.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+# get response of Downloadly web page
+# https://downloadly.ir/page/259/
+BASE_URL = 'https://downloadly.ir/'
+
+courses_title_dict = []
+
+filename = 'courses.csv'
+headers = ['title', 'link', 'image']
+
+#This is just for 5 page, number of downloadly site lage is 256
+# you can change rang betwwen 1 to 260
+for i in range(1,5) :
+    URL_TO_SCRAPE = "{BASE_URL}page/{p_no}/".format(BASE_URL=BASE_URL, p_no=i)
+
+    print("Start Scraping URL :" + URL_TO_SCRAPE)
+
+    r = requests.get(URL_TO_SCRAPE)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    courses = soup.select('#us_grid_1 .align_right')
+
+    
+
+    for item in courses :
+        row = {}
+        title = item.a
+        row['title'] = item.get_text()
+        row['link'] = item.a['href']
+        row['image'] = item.img['src']
+        courses_title_dict.append(row)
+
+    print("Finished Scraping URL : " + URL_TO_SCRAPE)
+
+
+with open(filename, 'w', newline='', encoding="utf-8") as f:
+    w = csv.DictWriter(f,headers)
+    w.writeheader()
+    for item in courses_title_dict :
+            w.writerow(item)
